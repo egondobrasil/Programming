d1d57f949c08c15f069c87c40c6993f1eee12d02
Egon <egondobrasil@gmail.com>
2026-01-19 09:12:36 -0300

    inserida variavel salario

--- primeiro.py
+++ primeiro.py
@@ -6,3 +6,7 @@ sobrenome='Witte'
 print('Seja bem-vindo '+nome+' '+sobrenome+'!')
 ano_nascimento=1986
 print('Você tem ' + str(2026-ano_nascimento) + ' anos de idade.')
+
+salario=1850.25
+reajustado=salario*1.1
+print(reajustado)
