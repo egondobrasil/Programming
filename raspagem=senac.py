f3bdac019a99d593edc08f413ecb30253638fb68
egondobrasil <egondobrasil@gmail.com>
2026-01-26 10:23:32 -0300

    Tentativa de conseguir mais dados com a raspagem.

--- raspagem=senac.py
+++ raspagem=senac.py
@@ -15,10 +15,8 @@ soup = BeautifulSoup(resp.text, 'html.parser')  # Analisar o conteúdo HTML da p
 h1 = soup.select_one('h1.ssp-card-detalhe-curso__title')
 print(h1.get_text(strip=True)if h1 else 'Sem H1')  # Extrair e exibir o texto do H1, ou 'Sem H1' se não houver H1 na página
 
-# Todos os parágrafos da página
 for p in soup.select('p.ssp-card-detalhe-curso__secundary-info-carga-horaria'):
     print(p.get_text(strip=True))  # Extrair e exibir o texto de cada parágrafo
 
-
-h6 = soup.select_one('h6.ssp-card-oferta-curso__item-valor-investimento')
-print(h6.get_text(strip=True)if h6 else 'Sem H6')  # Extrair e exibir o texto do H6, ou 'Sem H6' se não houver H6 na página
+for div in soup.select('div.ssp-saiba-mais-curso-accordion__heading'):
+    print(div.get_text(strip=True))  # Extrair e exibir o texto de cada parágrafo
