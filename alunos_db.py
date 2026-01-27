c50510d105eb210fa3937bbdcb7323138ce37536
egondobrasil <egondobrasil@gmail.com>
2026-01-27 11:13:28 -0300

    Concluído o código para manipulação de bancos de dados em MySQL.

--- alunos_db.py
+++ alunos_db.py
@@ -38,18 +38,42 @@ def adicionar_aluno(nome,email,celular,rg):
     conn.commit() # Confirma a transação
     print("Aluno adicionado com sucesso!")
     conn.close()
-
-#Função listar alunos
+ 
+ # Função listar alunos
 def listar_alunos():
     conn = conectar()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM alunos") #SQL Query para a consulta
-    alunos = cursor.fetchall() # Pegar todos os dados de alunos
-    print("Lista de Alunos")    
+    #sql para a consulta
+    cursor.execute("SELECT * FROM alunos")
+    alunos = cursor.fetchall() # Pegar todos os alunos
+    print("Lista de Alunos")
     for aluno in alunos:
-        print(f"ID: {aluno[0]} | Nome: {aluno[1]} | E-mail: {aluno[2]} | Celular{aluno[3]} | RG: {aluno[4]}")
+        print(f"ID: {aluno[0]} | Nome: {aluno[1]} | E-Mail: {aluno[2]} | Celular: {aluno[3]} | RG: {aluno[4]}")
     conn.close()
-
+ 
+# Função para atualizar o aluno
+def atualizar_aluno(id_aluno, nome, email, celular, rg):
+    conn = conectar()
+    cursor = conn.cursor()
+    #sql para update no registro
+    cursor.execute("""
+        UPDATE alunos set nome_aluno = %s, email = %s, celular = %s, rg = %s
+        WHERE id_aluno = %s                  
+     """, (nome, email, celular, rg, id_aluno))
+    conn.commit()
+    print(f"Aluno com o ID {'id_aluno'} atualizado com sucesso!")
+    conn.close()
+ 
+#Função de excluir registro
+def excluir_aluno(id_aluno):
+    conn = conectar()
+    cursor = conn.cursor()
+    #SQL para exclusão
+    cursor.execute("DELETE FROM alunos where id_aluno = %s", (id_aluno,))
+    conn.commit()
+    print(f"Aluno com o ID {id_aluno} excluído com sucesso!")
+    conn.close()
+ 
 def menu():
     while True:
         print("\n1. Criar Tabela")
@@ -71,10 +95,19 @@ def menu():
         elif escolha == "3":
             listar_alunos()
         elif escolha == "4":
-            print('Opção 4')
+            listar_alunos()
+            id_aluno = int(input("ID do aluno a ser atualizado: "))
+            nome = input("Nome: ")
+            email = input("E-mail: ")
+            celular = input("Celular: ")
+            rg = input("RG: ")
+            atualizar_aluno(id_aluno,nome,email,celular,rg)
         elif escolha == "5":
-            print('opção 5')
+            listar_alunos()
+            id_aluno = int(input("ID do aluno a ser excluido: "))
+            excluir_aluno(id_aluno)
         elif escolha == "6":
+            print("Saindo do sistema.")
             break
         else:
             print("Opção Invalida")
